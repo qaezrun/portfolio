0025db6d4029c2f7f89b61be88c0b9426ad08829
qaezrun <paezdanniel.vitto@gmail.com>
2023-07-28 01:14:23 +0800

    final commit for tonight, shadcn does not work right now sad.

--- src/components/pop.js
+++ src/components/pop.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './pop.css';
 
+/*import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '../../components/ui/alert-dialog'*/
+
 function Pop(props) {
     return (props.trigger) ? (
+        /*<AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+            <AlertDialogTitle>{props.h4}</AlertDialogTitle>
+            <AlertDialogDescription>
+                {props.p}
+            </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+            <AlertDialogAction>{props.text}</AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+        </AlertDialog>*/
         <div className='main'>
             <div className='sub-pop'>
                 {props.children}
