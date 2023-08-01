3efc90a38a154831849eb0dc605f1e34724e8e06
qaezrun <paezdanniel.vitto@gmail.com>
2023-08-01 18:08:36 +0800

    Fixed some naming

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Create from './components/MessageCreation/create';
-import Reviews from './components/Reviews/reviews';
+import Down from './components/WebPageDown/Down';
 
 function App() {
   return(
     <div className="App">
       <Routes>
         <Route path="/" element={ <Create/> } />
-        <Route path="reviews" element={ <Reviews/> } />
+        <Route path="Down" element={ <Down/> } />
       </Routes>
     </div>
   );
