bc4f60c6f367d2cf38bf798078b872f927e4d6f6
qaezrun <paezdanniel.vitto@gmail.com>
2023-08-01 18:03:19 +0800

    Added schemas for creating new email for users

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -3,10 +3,17 @@ const Schema = mongoose.Schema
 
 const userMessages = new Schema ({
     message:{type:String},
+    codeName:{type:String},
+    entryDate:{type:Date, default:Date.now}
+})
+
+const userToNotify = new Schema ({
+    email:{type:String},
     entryDate:{type:Date, default:Date.now}
 })
 
 const Messages = mongoose.model('Messages',userMessages,'messages')
-const mySchemas ={'Messages':Messages}
+const Emails = mongoose.model('Emails',userToNotify,'emails')
+const mySchemas ={'Messages':Messages,'Emails':Emails}
 
 module.exports = mySchemas
