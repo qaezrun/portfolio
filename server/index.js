32c3d0b971d125789222eefeb0eed0bbd11aa6ca
qaezrun <paezdanniel.vitto@gmail.com>
2023-12-28 23:02:46 +0800

    Special Branch For Server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./routes/router');
+const mongoose = require("mongoose")
+require('dotenv/config')
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+const corsOption = {
+    origin:"*",
+    credential: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOption))
+app.use('/',router)
+
+
+
+const dbOptions ={maxPoolSize:50,wtimeoutMS:2500,useNewUrlParser:true}
+mongoose.connect(process.env.DB_URI,dbOptions)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err))
+
+const port = 4000
+const server = app.listen(port,()=>{
+    console.log( `Server is running on port ${port}`)
+})
---
+++ server/routes/router.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = express.Router()
+const schemas = require('../models/schemas')
+
+router.get('/', async(req,res)=>{// this is how to get data from collection mongoose
+    res.send("Server is up and running!");
+})
+
+router.post('/messages',async (req,res)=>{
+    const {message,codeName} = req.body
+    
+    const data = {message:message,codeName:codeName}
+    const newMessage = new schemas.Messages(data)
+    const saveMessage = await newMessage.save()
+    if(saveMessage){
+        res.send({
+            header:"Thank you for sparing your time!",
+            desc:"Your participation is well appreciated. Click proceed to view other content I have created.",
+            btn:"Proceed"
+        })
+    }else{
+        res.send({
+            header:"Apologies, fatal error occured!",
+            desc:"I hope Danniel is aware of this and can fix it soon. Please try again later.",
+            btn:"Ok!"
+        })
+    }
+    res.end()
+})
+router.get('/userMessages', async(req,res)=>{// this is how to get data from collection mongoose
+    const messages = schemas.Messages //look on model/schemas.js Message is a model
+    
+    const userMessages = await messages.find({}).exec()// gets data from messages collection
+    if(userMessages){
+        res.send(JSON.stringify(userMessages))
+    }
+})
+
+
+router.post('/emails',async (req,res)=>{
+    const {email} = req.body
+
+    const data = {email:email}
+    const newEmail = new schemas.Emails(data)
+    const saveEmail = await newEmail.save()
+    if(saveEmail){
+        res.send({
+            header:"Thank you for your patience.",
+            desc:"Once the content is updated, you will receive a notification. See you later!",
+            btn:"Ok!"
+        })
+    }else{
+        res.send({
+            header:"Apologies, fatal error occured!",
+            desc:"I hope Danniel is aware of this and can fix it soon. Please try again later.",
+            btn:"Ok!"
+        })
+    }
+    res.end()
+})
+
+router.get('/userEmails', async(req,res)=>{// this is how to get data from collection mongoose
+    const emails = schemas.Emails //look on model/schemas.js Message is a model
+    
+    const userEmails = await emails.find({}).exec()// gets data from messages collection
+    if(userEmails){
+        res.send(JSON.stringify(userEmails))
+    }
+})
+
+module.exports = router
